99148658a1959fd886c2e2cfd48e67eefc2ed112
Kartik Sharma <kartikgautam1106@gmail.com>
2025-01-17 09:50:08 +0530

    Add files via upload

---
+++ cypress/e2e/Test Scripts/VerifyAllLinks.cy.js
@@ -0,0 +1,32 @@
+// Imports LoginPage class for actions related to the login page.
+
+import LoginPage from "../pages/page_actions/LoginPageAction.cy.js";
+// Imports HomePage class for actions related to the homepage.
+import HomePage from "../pages/page_actions/HomePageAction.cy.js";
+
+
+// Creating instances of the imported page classes for use in the tests.
+const loginAction = new LoginPage();
+const homepageAction = new HomePage();
+
+// import data from fixture files
+//const loginData = require('../fixtures/LoginData.json');
+const loginData = require('E:\PUMA CYPRESS E2E AUTOMATION\cypress\fixtures\TestDataFile\LoginData.json')
+describe("puma website", () => {
+
+    before(() => {
+        loginAction.visitLoginPage(loginData.url)// Navigates to the login page.
+        loginAction.fillEmail(loginData.email)// Enters the email for login.
+        loginAction.fillPassword(loginData.password)// Enters the password for login.
+        loginAction.isLoginVisible()//Check login button is exist and visible
+        loginAction.clickOnLogin()// Clicks the login button to submit the login form.
+    });
+    it('Verify all the Navigation links Present in NavBar is working', () => {
+        homepageAction.visitHomepage()//Navaigate to homepage
+        homepageAction.isAllNavbarLinksWorking();//check all the navigation bar links are working
+    })
+    it('Verify all the Footer links are working', () => {
+        homepageAction.isAllFooterLinksWorking()//check all the footer links are working
+    })
+
+  })
---
+++ cypress/pages/page_actions/CartPageAction.cy.js
@@ -0,0 +1,23 @@
+const cartLocators = require('../page_elements/CartPageElements.json')
+class CartPageAction {
+
+  // Method to visit cart page
+    visitCart(){
+        cy.get(cartLocators.cart).click()
+    }
+
+    // Method to setProduct quantity
+    setProductQuantity(quantity){
+        cy.xpath(cartLocators.quantity).select(quantity)
+    }
+  
+    //Method for proceed to checkout
+    proceedToCheckout() {
+      cy.get(cartLocators.checkout).click()
+    }
+
+
+  
+  }
+  
+  export default CartPageAction;
